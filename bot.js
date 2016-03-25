cef53b32d9cda74f4dd6a0351d6ffd1be654e5fc
brad <bradhaas@brads-mbp.gateway.sonic.net>
2016-03-25 13:53:09 -0700

    pipeline test

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = 'yeet';
+  botResponse = 'asdf';
 
   options = {
     hostname: 'api.groupme.com',
